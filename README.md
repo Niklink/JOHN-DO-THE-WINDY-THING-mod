602f98bee113dfa195a29e3a7b505dcb791a2c99
Niklink <niklink413@gmail.com>
2022-05-17 21:52:18 -0400

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# JOHN-DO-THE-WINDY-THING-mod
-Mod for The Unofficial Homestuck Collection. John does the windy thing.
+# JOHN DO THE WINDY THING
+Mod for [The Unofficial Homestuck Collection](https://bambosh.github.io/unofficial-homestuck-collection/) V2 or later. (Tested on V2.0.5)
+
+By activating the four corner buttons in Dave's Phat Beat Machine, you reveal secret tracks to play around with. These tracks were externally hosted and eventually broke. In early 2019, through the death of Flash in 2020, fans managed to sneakily purchase one of the expired domains and replaced the music with a new track. This mod restores this unofficial behavior. Music by Robert J! Lake.
---
+++ mod.js
@@ -0,0 +1,14 @@
+module.exports = {
+	
+	title: "JOHN DO THE WINDY THING", 
+	summary: "Restores the hijacked Phat Beat Machine music",
+	author: "Niklink",
+	modVersion: "1",
+	description: "By activating the four corner buttons in <a href=\"assets://archive/homestuck/338\">Dave's Phat Beat Machine</a>, you reveal secret tracks to play around with. These tracks were externally hosted and eventually broke. In early 2019, through the death of Flash in 2020, fans managed to sneakily purchase one of the expired domains and replaced the music with a new track. This mod restores this unofficial behavior. Music by Robert J! Lake.",    
+	locked: "002238",
+  
+    routes: {
+        'assets://storyfiles/hs2/00338/Aerosmith_-_I_Dont_Wanna_Miss_A_Thing.mp3': './Aerosmith_-_I_Dont_Wanna_Miss_A_Thing.mp3'
+    }
+
+}
