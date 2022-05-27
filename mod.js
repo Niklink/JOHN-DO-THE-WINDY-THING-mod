364d9f78b1e9cef2f611481b22d41f441101aa7c
Niklink <niklink413@gmail.com>
2022-05-27 09:20:50 -0400

    fix description link

--- mod.js
+++ mod.js
@@ -3,8 +3,8 @@ module.exports = {
 	title: "JOHN DO THE WINDY THING", 
 	summary: "Restores the hijacked Phat Beat Machine music",
 	author: "Niklink",
-	modVersion: "1",
-	description: "By activating the four corner buttons in <a href=\"assets://archive/homestuck/338\">Dave's Phat Beat Machine</a>, you reveal secret tracks to play around with. These tracks were externally hosted and eventually broke. In early 2019, through the death of Flash in 2020, fans managed to sneakily purchase one of the expired domains and replaced the music with a new track. This mod restores this unofficial behavior. Music by Robert J! Lake.",    
+	modVersion: "1.1",
+	description: "By activating the four corner buttons in <a href=\"/story/338\" target=\"_blank\">Dave's Phat Beat Machine</a>, you reveal secret tracks to play around with. These tracks were externally hosted and eventually broke. In early 2019, through the death of Flash in 2020, fans managed to sneakily purchase one of the expired domains and replaced the music with a new track. This mod restores this unofficial behavior. Music by Robert J! Lake.",    
 	locked: "002238",
   
     routes: {
